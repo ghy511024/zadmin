07ae9d1638371d4de1e0c73ade904c8a895a2bee
龚虹宇 <hongyu.gong@cloudacc-inc.com>
2017-05-11 01:16:44 +0800

    merge

---
+++ src/js/zadmin.all.js
@@ -0,0 +1,11 @@
+__inline("/src/js/tool/Ut.js");
+__inline("/src/js/tool/csstool.js");
+__inline("/src/js/tool/zen.js");
+__inline("/src/js/tool/ftip.js");
+__inline("/src/js/tool/zalert.js");
+__inline("/src/js/form/imgajaxupload.js");
+__inline("/src/js/form/zform.js");
+__inline("/src/js/tool/maskimage.js");
+__inline("/src/js/tool/core.js");
+
+//__inline("/lib/js/tool/coreV2.js");
