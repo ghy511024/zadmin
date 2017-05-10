cb450020435d156ffce01a439af828dd7628338c
龚虹宇 <gonghongyu@le.com>
2017-05-10 15:59:58 +0800

    merge

---
+++ dist/jstpl/popimg.js
@@ -0,0 +1,11 @@
+
+window.TPL = window.TPL || {};
+TPL.tplmap = TPL.tplmap || {};
+TPL.getTpl = TPL.getTpl || function (_id) {
+    return TPL.tplmap[_id];
+};
+
+(function (TPL) {
+    TPL.tplmap['zadmin-popimg'] = '<div class="overlay" style="display: none;"></div><div class="pop-modal"><i class="pop-close iconfont icon-close_m"></i><i class="pop-detail iconfont icon-menu"></i><div class="modal_content"><ul class="pop-list"></ul><img src="http://fengyun-bar.b0.upaiyun.com/1470742916289691.jpg"></div></div>';
+})(TPL);
+   
